cf5391aece3a8467ce7a2a64da08f1884f77b9c3
figsoda <figsoda@pm.me>
2020-08-20 15:14:17 -0400

    Reformat code

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,41 @@
-mod util;
-use util::{get_info, Config};
+mod types;
+use types::{Config, Info};
 
-use clap::clap_app;
 use failure::Fallible;
 use serde::Serialize;
+use structopt::StructOpt;
 use walkdir::WalkDir;
 
 use std::{fs, path::Path};
 
-fn app() -> Fallible<()> {
-    let args = clap_app!(pactorio =>
-        (version : "0.2.0")
-        (author : "figsoda <figsoda@pm.me>")
-        (about : "Factorio mod packer")
-        (@arg compact : -c --compact "Output info.json compactly")
-        (@arg input : -i --input [CONFIG_FILE] "Specify the config file to use")
-        (@arg output : -o --output [OUTPUT_DIRECTORY] "Specify the output directory")
-    )
-    .get_matches();
+/// Factorio mod packer
+#[derive(StructOpt)]
+#[structopt(name = "pactorio")]
+struct Opt {
+    /// Output info.json compactly
+    #[structopt(short, long)]
+    compact: bool,
 
-    let cfg: Config = toml::from_str(&fs::read_to_string(
-        args.value_of("input").unwrap_or("pactorio.toml"),
-    )?)?;
+    /// Specify the config file to use
+    #[structopt(short, long, default_value = "pactorio.toml")]
+    input: String,
+
+    /// Specify the output directory
+    #[structopt(short, long, default_value = "release")]
+    output: String,
+}
+
+fn main() -> Fallible<()> {
+    let opt = Opt::from_args();
+
+    let cfg: Config = toml::from_str(&fs::read_to_string(opt.input)?)?;
 
     let mut files = Vec::new();
     for entry in WalkDir::new(&cfg.source.dir).min_depth(1) {
         files.push(entry?.path().to_owned());
     }
 
-    let output = Path::new(args.value_of("output").unwrap_or("release")).join({
+    let output = Path::new(&opt.output).join({
         let mut name = String::new();
         name.push_str(&cfg.package.name);
         name.push('_');
@@ -54,23 +61,20 @@ fn app() -> Fallible<()> {
         }
     }
 
-    let info = get_info(cfg);
+    let info = Info::from(cfg);
     fs::write(
         output.join("info.json"),
-        if args.is_present("compact") {
+        if opt.compact {
             serde_json::to_string(&info)?
         } else {
             let mut writer = Vec::with_capacity(256);
-            let pretty = serde_json::ser::PrettyFormatter::with_indent(b"    ");
-            let mut ser = serde_json::Serializer::with_formatter(&mut writer, pretty);
-            info.serialize(&mut ser)?;
+            info.serialize(&mut serde_json::Serializer::with_formatter(
+                &mut writer,
+                serde_json::ser::PrettyFormatter::with_indent(b"    "),
+            ))?;
             String::from_utf8(writer)?
         },
     )?;
 
     Ok(())
-}
-
-fn main() {
-    let _ = app().map_err(|e| println!("{}", e));
 }
---
+++ src/types.rs
@@ -0,0 +1,105 @@
+use serde::{Deserialize, Serialize};
+
+use std::{collections::HashMap, default::Default};
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct Deps {
+    #[serde(default)]
+    required: HashMap<String, String>,
+    #[serde(alias = "incompatible")]
+    #[serde(default)]
+    conflict: HashMap<String, String>,
+    #[serde(default)]
+    optional: HashMap<String, String>,
+    #[serde(default)]
+    hidden: HashMap<String, String>,
+}
+
+#[derive(Clone, Debug, Deserialize)]
+pub struct Package {
+    pub name: String,
+    pub version: String,
+    pub title: String,
+    pub author: String,
+    pub contact: Option<String>,
+    pub homepage: Option<String>,
+    pub description: Option<String>,
+    pub factorio_version: Option<String>,
+}
+
+#[derive(Clone, Debug, Deserialize)]
+pub struct Source {
+    #[serde(alias = "directory")]
+    pub dir: String,
+}
+
+impl Default for Source {
+    fn default() -> Source {
+        Source {
+            dir: String::from("src"),
+        }
+    }
+}
+
+#[derive(Clone, Debug, Deserialize)]
+pub struct Config {
+    pub package: Package,
+    #[serde(alias = "dependencies")]
+    pub deps: Option<Deps>,
+    #[serde(default)]
+    pub source: Source,
+}
+
+#[derive(Clone, Debug, Serialize)]
+pub struct Info {
+    pub name: String,
+    pub version: String,
+    pub title: String,
+    pub author: String,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub contact: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub homepage: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub description: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub factorio_version: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub dependencies: Option<Vec<String>>,
+}
+
+impl From<Config> for Info {
+    fn from(cfg: Config) -> Self {
+        Info {
+            name: cfg.package.name,
+            version: cfg.package.version,
+            title: cfg.package.title,
+            author: cfg.package.author,
+            contact: cfg.package.contact,
+            homepage: cfg.package.homepage,
+            description: cfg.package.description,
+            factorio_version: cfg.package.factorio_version,
+            dependencies: cfg.deps.map(|deps| {
+                let mut xs = Vec::new();
+
+                for (name, version) in deps.required {
+                    xs.push(format!("{} {}", name, version));
+                }
+
+                for (name, version) in deps.conflict {
+                    xs.push(format!("! {} {}", name, version));
+                }
+
+                for (name, version) in deps.optional {
+                    xs.push(format!("? {} {}", name, version));
+                }
+
+                for (name, version) in deps.hidden {
+                    xs.push(format!("(?) {} {}", name, version));
+                }
+
+                xs
+            }),
+        }
+    }
+}
