677eae52f229ba7ffd43a0bd1d97e502a63fc047
figsoda <figsoda@pm.me>
2020-09-04 12:20:51 -0400

    Option to output zip files instead of folders

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,13 @@ use anyhow::{Context, Result};
 use serde::Serialize;
 use structopt::StructOpt;
 use walkdir::WalkDir;
+use zip::{write::FileOptions, CompressionMethod, ZipWriter};
 
-use std::{fs, path::Path};
+use std::{
+    fs::{self, File},
+    io::{self, Write},
+    path::Path,
+};
 
 /// Factorio mod packager https://github.com/figsoda/pactorio
 #[derive(StructOpt)]
@@ -23,6 +28,10 @@ struct Opt {
     /// Specify the output directory
     #[structopt(short, long, default_value = "release")]
     output: String,
+
+    /// Output a zip file instead
+    #[structopt(short, long)]
+    zip: bool,
 }
 
 fn main() -> Result<()> {
@@ -47,50 +56,91 @@ fn main() -> Result<()> {
         );
     }
 
-    let output =
-        Path::new(&opt.output).join(format!("{}_{}", cfg.package.name, cfg.package.version));
+    let info = Info::from(cfg.clone());
+    let info = if opt.compact {
+        serde_json::to_vec(&info).context("Failed to generate info.json")?
+    } else {
+        let mut writer = Vec::with_capacity(256);
+        info.serialize(&mut serde_json::Serializer::with_formatter(
+            &mut writer,
+            serde_json::ser::PrettyFormatter::with_indent(b"    "),
+        ))
+        .context("Failed to generate info.json")?;
+        writer
+    };
 
-    if output.is_dir() {
-        fs::remove_dir_all(&output)
-            .context(format!("Failed to remove directory {}", output.display()))?;
-    } else if output.is_file() {
-        fs::remove_file(&output).context(format!("Failed to remove file {}", output.display()))?;
-    }
-    fs::create_dir_all(&output)
-        .context(format!("Failed to create directory {}", output.display()))?;
+    if opt.zip {
+        fs::create_dir_all(&opt.output)
+            .context(format!("Failed to create directory {}", &opt.output))?;
+        let output = Path::new(&opt.output)
+            .join(format!("{}_{}.zip", cfg.package.name, cfg.package.version));
+        if output.is_dir() {
+            fs::remove_dir_all(&output)
+                .context(format!("Failed to remove directory {}", output.display()))?;
+        } else if output.is_file() {
+            fs::remove_file(&output)
+                .context(format!("Failed to remove file {}", output.display()))?;
+        }
 
-    for from in files {
-        if let Ok(to) = from.strip_prefix(&cfg.source.dir) {
-            let to = output.join(to);
-            if from.is_dir() {
-                fs::create_dir(&to)
-                    .context(format!("Failed to create directory {}", to.display()))?;
-            } else if from.is_file() {
-                fs::copy(&from, &to).context(format!(
-                    "Failed to copy from {} to {}",
-                    from.display(),
-                    to.display(),
-                ))?;
+        let mut zip = ZipWriter::new(
+            File::create(&output)
+                .context(format!("Failed to create zip file {}", output.display()))?,
+        );
+        let fo = FileOptions::default()
+            .compression_method(CompressionMethod::Stored)
+            .unix_permissions(0o755);
+
+        for from in files {
+            if let Ok(to) = from.strip_prefix(&cfg.source.dir) {
+                if from.is_dir() {
+                    zip.add_directory_from_path(to, Default::default())
+                        .context("Failed to write to the zip file")?;
+                } else if from.is_file() {
+                    let mut file = File::open(&from)
+                        .context(format!("Failed to read file {}", from.display()))?;
+                    zip.start_file_from_path(to, fo)
+                        .context("Failed to write to the zip file")?;
+                    io::copy(&mut file, &mut zip).context("Failed to write to the zip file")?;
+                }
             }
         }
-    }
 
-    let info = Info::from(cfg);
-    fs::write(
-        output.join("info.json"),
-        if opt.compact {
-            serde_json::to_string(&info).context("Failed to generate info.json")?
-        } else {
-            let mut writer = Vec::with_capacity(256);
-            info.serialize(&mut serde_json::Serializer::with_formatter(
-                &mut writer,
-                serde_json::ser::PrettyFormatter::with_indent(b"    "),
-            ))
-            .context("Failed to generate info.json")?;
-            String::from_utf8(writer).context("Failed to generate info.json")?
-        },
-    )
-    .context("Failed to create file info.json")?;
+        zip.start_file("info.json", fo)
+            .context("Failed to write to the zip file")?;
+        zip.write_all(&info)
+            .context("Failed to write to the zip file")?;
+        zip.finish().context("Failed to write to the zip file")?;
+    } else {
+        let output =
+            Path::new(&opt.output).join(format!("{}_{}", cfg.package.name, cfg.package.version));
+        if output.is_dir() {
+            fs::remove_dir_all(&output)
+                .context(format!("Failed to remove directory {}", output.display()))?;
+        } else if output.is_file() {
+            fs::remove_file(&output)
+                .context(format!("Failed to remove file {}", output.display()))?;
+        }
+        fs::create_dir_all(&output)
+            .context(format!("Failed to create directory {}", output.display()))?;
+
+        for from in files {
+            if let Ok(to) = from.strip_prefix(&cfg.source.dir) {
+                let to = output.join(to);
+                if from.is_dir() {
+                    fs::create_dir(&to)
+                        .context(format!("Failed to create directory {}", to.display()))?;
+                } else if from.is_file() {
+                    fs::copy(&from, &to).context(format!(
+                        "Failed to copy from {} to {}",
+                        from.display(),
+                        to.display(),
+                    ))?;
+                }
+            }
+        }
+
+        fs::write(output.join("info.json"), info).context("Failed to create file info.json")?;
+    }
 
     Ok(())
 }
