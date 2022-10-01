8f1cae9fb5738adbae59bb329af5b3089350c321
figsoda <figsoda@pm.me>
2022-10-01 18:37:45 -0400

    generate man pages

--- build.rs
+++ build.rs
@@ -1,22 +1,29 @@
 use clap::CommandFactory;
 use clap_complete::{generate_to, Shell};
+use clap_mangen::Man;
 
-use std::{env, fs::create_dir_all, path::Path};
+use std::{
+    env,
+    fs::{create_dir_all, File},
+    path::Path,
+};
 
 include!("src/cli.rs");
 
 fn main() {
-    println!("cargo:rerun-if-env-changed=GEN_COMPLETIONS");
+    println!("cargo:rerun-if-env-changed=GEN_ARTIFACTS");
 
-    if env::var_os("GEN_COMPLETIONS") != Some("1".into()) {
-        return;
-    }
+    if let Some(dir) = env::var_os("GEN_ARTIFACTS") {
+        let out = &Path::new(&dir);
+        create_dir_all(out).unwrap();
+        let cmd = &mut Opts::command();
 
-    let out = &Path::new(&env::var_os("OUT_DIR").unwrap()).join("completions");
-    create_dir_all(out).unwrap();
-    let app = &mut Opts::command();
+        Man::new(cmd.clone())
+            .render(&mut File::create(out.join("pactorio.1")).unwrap())
+            .unwrap();
 
-    for shell in Shell::value_variants() {
-        generate_to(*shell, app, "pactorio", out).unwrap();
+        for shell in Shell::value_variants() {
+            generate_to(*shell, cmd, "pactorio", out).unwrap();
+        }
     }
 }
